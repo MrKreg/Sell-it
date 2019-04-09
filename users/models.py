f808684d2c698214efb4cf945495a4003fea7d40
MrKreg <taras.sheketa@gmail.com>
2019-04-09 18:29:31 +0300

    Added phone field to user

--- users/models.py
+++ users/models.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+from Sell_it.fields import PhoneField
 from users.choices import GENDER_CHOICES
 
 
@@ -8,3 +9,4 @@ class User(AbstractUser):
     birth_date = models.DateField(null=True)
     gender = models.CharField(max_length=10, choices=GENDER_CHOICES)
     email = models.EmailField(unique=True)
+    phone = PhoneField()
