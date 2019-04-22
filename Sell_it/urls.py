d165fbf990f92b9d8dd250c8aa3980ce036f835d
MrKreg <taras.sheketa@gmail.com>
2019-04-22 16:44:37 +0300

    Added Swagger API doc to project

--- Sell_it/urls.py
+++ Sell_it/urls.py
@@ -1,9 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework_swagger.views import get_swagger_view
+
+schema_view = get_swagger_view(title='Agora API')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('users/', include('users.urls', namespace='users')),
     path('password_reset/',
          include('django_rest_passwordreset.urls', namespace='password_reset')),
+    path('', schema_view),
 ]
