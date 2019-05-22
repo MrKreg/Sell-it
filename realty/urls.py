5abcda9e8a8b2541cf374586cf71daeb555dd7fb
MrKreg <taras.sheketa@gmail.com>
2019-05-22 22:35:05 +0300

    Added like system

--- realty/urls.py
+++ realty/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from rest_framework.routers import SimpleRouter
 
-from realty.views import RealtyViewSet, RealtyPhotoViewSet
+from realty.views import RealtyViewSet, RealtyPhotoViewSet, LikedRealtyViewSet
 
 __all__ = ['urlpatterns', ]
 
@@ -10,5 +10,6 @@ app_name = 'realty'
 router = SimpleRouter()
 router.register(r'default', RealtyViewSet, basename='default')
 router.register(r'photos', RealtyPhotoViewSet, basename='photos')
+router.register(r'likes', LikedRealtyViewSet, basename='likes')
 
 urlpatterns = router.urls
--- realty/views.py
+++ realty/views.py
@@ -1,6 +1,7 @@
 from django.db.models import Q
-from rest_framework import viewsets
+from rest_framework import viewsets, status
 from rest_framework import mixins
+from rest_framework.response import Response
 
 from realty.filters import RealtyFilter
 from realty.models import Realty, RealtyPhoto
@@ -24,8 +25,39 @@ class RealtyViewSet(viewsets.ModelViewSet):
         self.serializer_class = RealtyListPolymorphicSerializer
         return super().list(request, args, kwargs)
 
+    def perform_create(self, serializer):
+        serializer.save(creator=self.request.user)
+
 
 class RealtyPhotoViewSet(mixins.CreateModelMixin, mixins.DestroyModelMixin,
                          viewsets.GenericViewSet):
     queryset = RealtyPhoto.objects.all()
     serializer_class = RealtyPhotoSerializer
+
+
+class LikedRealtyViewSet(mixins.UpdateModelMixin, mixins.DestroyModelMixin,
+                         mixins.ListModelMixin, viewsets.GenericViewSet):
+    queryset = Realty.objects.all()
+    serializer_class = RealtyListPolymorphicSerializer
+
+    def list(self, request, *args, **kwargs):
+        queryset = self.filter_queryset(self.get_queryset())
+        queryset = queryset.filter(user=request.user)
+
+        page = self.paginate_queryset(queryset)
+        if page is not None:
+            serializer = self.get_serializer(page, many=True)
+            return self.get_paginated_response(serializer.data)
+
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
+
+    def update(self, request, *args, **kwargs):
+        instance = self.get_object()
+        instance.user_set.add(request.user)
+        return Response(status=status.HTTP_200_OK)
+
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        instance.user_set.remove(request.user)
+        return Response(status=status.HTTP_200_OK)
