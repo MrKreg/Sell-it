46f25b8b675d539021658a9d25fde1708ef6dde0
Taras Sheketa <taras.sheketa@gmail.com>
2019-06-06 23:24:21 +0300

    Api/user (#12)
    * Added details realties view
    
    * Changed filters for users to search

--- users/filters.py
+++ users/filters.py
@@ -1,4 +1,5 @@
 import django_filters
+from django.db.models import Q
 
 from users.models import User
 
@@ -6,10 +7,9 @@ __all__ = ['UserFilter', ]
 
 
 class UserFilter(django_filters.FilterSet):
-    class Meta:
-        model = User
-        fields = {
-            'first_name': ['icontains', ],
-            'last_name': ['icontains', ],
-            'phone': ['icontains', ],
-        }
+    search = django_filters.CharFilter(method='search_filter')
+
+    def search_filter(self, queryset, name, value):
+        return queryset.filter(Q(username__icontains=value)
+                               | Q(first_name__icontains=value)
+                               | Q(last_name__icontains=value))
